af5a49c5dc8595189b7c00921d20c313d5e6093d
Solid-star143 <2165619@cognizant.com>
2024-12-25 19:45:34 +0530

    Modified  Operators File

--- Operators.js
+++ Operators.js
@@ -1,6 +1,14 @@
-console.log(10+20);
-console.log(2**3);//8
-var a = 10;
-console.log(a++);//Post Incrementation - 10 But internal a is 11
-console.log(a);//a = 11
-console.log(++a);//Pre Incrementation - 12
+// console.log(10+20);
+// console.log(2**3);//8
+// var a = 10;
+// console.log(a++);//Post Incrementation - 10 But internal a is 11
+// console.log(a);//a = 11
+// console.log(++a);//Pre Incrementation - 12
+
+//== Checks only value not data type:
+console.log(10 == 10); //true
+console.log(10 == '10');//true
+
+//=== Checks both value and data type:
+console.log(10 === '10');//false : Values are matching but not data type
+console.log(10 == 10);//true
