5b9790d8e2d76d957b1f98f8f335f75b8227beb5
Solid-star143 <2165619@cognizant.com>
2024-12-25 23:52:13 +0530

    Added Functions File

---
+++ Functions.js
@@ -0,0 +1,36 @@
+// 4 Types of Functions:
+
+//1.Functions without input and without return:
+function add(){
+    var a = 10;
+    var b = 20;
+    console.log("Addition is: ", a+b);
+}
+add();
+
+
+//2.Functions with input and without return:
+function sub(x,y){
+    console.log("Subtraction is: ", x-y);//100
+}
+sub(200,100);
+
+ 
+//3.Functions without input and with return:
+function mul(){
+    var p = 300;
+    var q = 100;
+    return p * q;
+}
+let result = mul();
+console.log("Multiplication is: ", result);
+
+
+//4.Functions with input and with return:
+function div(a,b){
+    return a/b;
+}
+console.log("This Division is: ",div(500,10))
+
+
+
