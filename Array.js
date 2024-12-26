d3399ad433363b22f6efd31d53849b3b4df30052
Solid-star143 <2165619@cognizant.com>
2024-12-26 21:09:03 +0530

    Diff_ways_of_Function_Declaration.js

---
+++ Array.js
@@ -0,0 +1,8 @@
+var arr1 = [10,20,30,40];
+// console.log('Element at 0th index:',arr1[0]);
+// console.log('Element at 1st index:',arr1[1]);
+// console.log('Element at 2nd index:',arr1[2]);
+// console.log('Element at 3rd index:',arr1[3]);
+
+
+for(var i=1;arr1<=i;i++);
