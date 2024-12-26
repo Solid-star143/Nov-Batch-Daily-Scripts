05a70cd52b0d7004fc5167615de71b35afa79b37
Solid-star143 <2165619@cognizant.com>
2024-12-26 22:05:51 +0530

    Added Array.js

--- Array.js
+++ Array.js
@@ -1,8 +1,16 @@
-var arr1 = [10,20,30,40];
+var arr1 = [10,50.45,true,'Kod',{name: 'Priya', age: 45},()=>{console.log('Function')},[100,200]];
+console.log('The length of arr1 array:',arr1.length);
+
+for(var i=0;i<=arr1.length-1;i++) {
+    console.log(`Element at index ${i}`, arr1[i]);
+}
+
+console.log(arr1, typeof arr1);
+
+// Array can store Homogenous(Data with same Data type) as well as Heterogenous(Data with diff data type)
 // console.log('Element at 0th index:',arr1[0]);
 // console.log('Element at 1st index:',arr1[1]);
 // console.log('Element at 2nd index:',arr1[2]);
 // console.log('Element at 3rd index:',arr1[3]);
 
 
-for(var i=1;arr1<=i;i++);
---
+++ FunctionObject.js
@@ -0,0 +1,9 @@
+function greet(){
+    console.log('Hello');
+}
+
+console.log(greet, typeof greet);
+
+// Functions are objects in Javascript
+
+console.log(typeof null);
