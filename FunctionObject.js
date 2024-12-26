bf63453dab6a6f148af9d06b7d25499964222918
Solid-star143 <2165619@cognizant.com>
2024-12-26 22:46:31 +0530

    Object Files

--- FunctionObject.js
+++ FunctionObject.js
@@ -6,4 +6,19 @@ console.log(greet, typeof greet);
 
 // Functions are objects in Javascript
 
-console.log(typeof null);
+function identifyDataType(value) {
+    if (value === null) {
+        return 'Null';
+    }
+    if (Array.isArray(value)){
+        return 'Array'
+    }
+        return typeof value;
+}
+
+res = identifyDataType([10,20,30]);
+// console.log(res);
+
+console.log(Array.isArray(null)); //false
+console.log(Array.isArray([10,20,30])); //true
+
