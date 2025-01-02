2a130f1ecbc80cd4c43abb45aa3ca4d01cc97e09
Solid-star143 <2165619@cognizant.com>
2025-01-02 11:19:41 +0530

    Added Function Overloading file

---
+++ FunctionOverloading.js
@@ -0,0 +1,15 @@
+add(10,20);//NaN
+add(10,20,30);//NaN
+add(10,20,30,40);//100
+
+function add(a,b){
+    console.log(a+b);
+}
+
+function add(a,b,c){
+    console.log(a+b+c);
+}
+
+function add(a,b,c,d){
+    console.log(a+b+c+d);
+}
