09c7dcc71f83425ff678b206bca776c998c52a09
Solid-star143 <2165619@cognizant.com>
2025-01-02 10:15:11 +0530

    Added Memory management file

--- memorymanagement.js
+++ memorymanagement.js
@@ -4,6 +4,7 @@ console.log(a); //20
 
 console.log(a); //Error
 
+
 const a = 30;
 //At the time of memory allocation,this a will be in TDZ but at the time of code
 //Execution this will be assigned with 30
